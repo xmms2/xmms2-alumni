c2ce15537b8223915480e3eaecaf0ad407e99516
Florian Ragwitz <rafl@debian.org>
2007-05-28 17:10:01 +0200

    Add some sample tests in perl and c as well as a small c header to make emiting tap easier.

---
+++ t/include/tap.h
@@ -0,0 +1,66 @@
+#ifndef __TAP_H__
+#define __TAP_H__
+
+#include <stdio.h>
+
+int tap_tests_planned = -1;
+int tap_tests_ran = 0;
+int tap_skip_num;
+char *tap_skip_reason;
+char *tap_todo_reason = NULL;
+
+#define PLAN_TESTS(x) \
+	tap_tests_planned = x; \
+	printf ("1..%d\n", x);
+
+#define OK1(test) \
+	OK(test, NULL);
+
+#define OK(test, desc) { \
+		tap_tests_ran++; \
+		int success = test; \
+		if (!success) { \
+			printf ("not "); \
+		} \
+		printf ("ok %d", tap_tests_ran); \
+		if (desc) \
+			printf (" - %s", desc); \
+		if (tap_todo_reason) \
+			printf (" # TODO %s", tap_todo_reason); \
+		printf ("\n"); \
+		if (!success) { \
+			printf ("#    Failed test "); \
+			if (desc) \
+				printf ("'%s' ", desc); \
+			printf ("in %s at line %d\n", __FILE__, __LINE__); \
+		} \
+	}
+
+#define EXIT_STATUS \
+	!(tap_tests_ran == tap_tests_planned)
+
+#define SKIP(num, desc) { \
+		int i = 0; \
+		while (i++ < num) { \
+			tap_tests_ran++; \
+			printf ("ok %d # skip %s\n", tap_tests_ran, desc); \
+		} \
+	}
+
+#define TODO_START(desc) \
+	tap_todo_reason = desc;
+
+#define TODO_END \
+	tap_todo_reason = NULL;
+
+#define SKIP_START(cond, num, desc) \
+	tap_skip_num = num; \
+	tap_skip_reason = desc; \
+	if (!cond) { \
+
+#define SKIP_END \
+	} else { \
+		SKIP(tap_skip_num, tap_skip_reason); \
+	}
+
+#endif /* __TAP_H__ */
