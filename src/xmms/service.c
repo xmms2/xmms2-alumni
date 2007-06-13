ce93859eebffd363907d2ccc66b9d8db9200de7c
Ning Shi <zeegeek@gmail.com>
2007-06-14 00:36:28 +0800

    OTHER: Add more service registration functions.

--- src/xmms/service.c
+++ src/xmms/service.c
@@ -52,12 +52,12 @@ typedef struct xmms_service_method_St {
 	GMutex *mutex;
 	GList *clients;
 
-	xmms_object_cmd_arg_type_t ret_type;
+	gchar *ret_type;
 
 	/* Number of arguments */
 	guint num_args;
-	/* List of types of arguments */
-	GList *args;
+	/* A string of types of arguments */
+	gchar *args;
 } xmms_service_method_t;
 
 static xmms_service_t *xmms_service;
@@ -69,9 +69,22 @@ static void xmms_service_destroy (xmms_object_t *object);
 static void xmms_service_registry_destroy (gpointer value);
 static void xmms_service_method_destroy (gpointer value);
 
-void xmms_service_handle (xmms_ipc_msg_t *msg, uint32_t cmdid,
-						  uint32_t cookie);
+void xmms_service_handle (xmms_ipc_msg_t *msg, uint32_t cmdid, uint32_t cookie);
+static gboolean xmms_service_is_registered (gchar *name);
+static gboolean xmms_service_is_method_registered (xmms_service_entry_t *entry,
+												   gchar *name);
 static void xmms_service_register (xmms_ipc_msg_t *msg, uint32_t cookie);
+static void xmms_service_method_register (xmms_ipc_msg_t *msg,
+										  xmms_service_entry_t *entry);
+static xmms_service_entry_t *xmms_service_entry_new (gchar *name,
+													 gchar *description,
+													 guint major, guint minor,
+													 guint cookie);
+static xmms_service_method_t *xmms_service_method_new (gchar *name,
+													   gchar *description,
+													   gchar *ret_type,
+													   guint num_args,
+													   gchar *args);
 
 /**
  * Initialize service client handling
@@ -144,7 +157,7 @@ xmms_service_method_destroy (gpointer value)
 	g_mutex_free (v->mutex);
 	g_list_free (v->clients);
 
-	g_list_free (v->args);
+	g_free (v->args);
 }
 
 /**
@@ -166,6 +179,7 @@ xmms_service_handle (xmms_ipc_msg_t *msg, uint32_t cmdid, uint32_t cookie)
 
 	switch (cmdid) {
 	case XMMS_IPC_CMD_SERVICE_REGISTER:
+		xmms_service_register (msg, cookie);
 		break;
 	case XMMS_IPC_CMD_SERVICE_REQUEST:
 		break;
@@ -182,9 +196,163 @@ xmms_service_handle (xmms_ipc_msg_t *msg, uint32_t cmdid, uint32_t cookie)
 static void
 xmms_service_register (xmms_ipc_msg_t *msg, uint32_t cookie)
 {
+	gchar *n;
+	guint l;
+	gchar *desc;
+	guint major, minor;
+
 	g_return_if_fail (msg);
 
 	XMMS_DBG ("Registering new service");
+
+	if (!xmms_ipc_msg_get_string_alloc (msg, &n, &l)) {
+		xmms_log_error ("No service name in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_string_alloc (msg, &desc, &l)) {
+		xmms_log_error ("No service description in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_uint32 (msg, &major)) {
+		xmms_log_error ("No major version in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_uint32 (msg, &minor)) {
+		xmms_log_error ("No minor version in message!");
+		return;
+	}
+
+	if (!xmms_service_is_registered (n)) {
+		xmms_log_error ("Service already registered!");
+		return;
+	}
+	xmms_service_entry_t *e;
+	e = xmms_service_entry_new (n, desc, major, minor, cookie);
+	g_mutex_lock (xmms_service->mutex);
+	g_hash_table_insert (xmms_service->registry, n, e);
+	g_mutex_unlock (xmms_service->mutex);
+
+	xmms_service_method_register (msg, e);
+}
+
+/**
+ * Register a new method
+ */
+static void
+xmms_service_method_register (xmms_ipc_msg_t *msg, xmms_service_entry_t *entry)
+{
+	gchar *n;
+	guint l;
+	gchar *desc;
+	gchar *rt, *at;
+
+	g_return_if_fail (msg);
+
+	XMMS_DBG ("Registering new method");
+
+	if (!xmms_ipc_msg_get_string_alloc (msg, &n, &l)) {
+		xmms_log_error ("No method name in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_string_alloc (msg, &desc, &l)) {
+		xmms_log_error ("No method description in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_string_alloc (msg, &rt, &l)) {
+		xmms_log_error ("No return types in message!");
+		return;
+	}
+	if (!xmms_ipc_msg_get_string_alloc (msg, &at, &l)) {
+		xmms_log_error ("No argument types in message!");
+		return;
+	}
+
+	if (!xmms_service_is_method_registered (entry, n)) {
+		xmms_log_error ("Method already registered!");
+		return;
+	}
+
+	gchar *c;
+	for (c = rt; *c; c++) {
+		switch (*c) {
+		case 'u':
+		case 'i':
+		case 's':
+		case 'l':
+		case 'c':
+		case 'b':
+			break;
+		default:
+			xmms_log_error ("Invalid return type (%c)!", *c);
+			return;
+		}
+	}
+	l = 0;
+	for (c = at; *c; c++, l++) {
+		switch (*c) {
+		case 'u':
+		case 'i':
+		case 's':
+		case 'l':
+		case 'c':
+		case 'b':
+			break;
+		default:
+			xmms_log_error ("Invalid argument type (%c)!", *c);
+			return;
+		}
+	}
+
+	xmms_service_method_t *m;
+	m = xmms_service_method_new (n, desc, rt, l, at);
+	g_mutex_lock (entry->mutex);
+	g_hash_table_insert (entry->methods, n, m);
+	g_mutex_unlock (entry->mutex);
+}
+
+static gboolean
+xmms_service_is_registered (gchar *name)
+{
+	gboolean ret = TRUE;
+
+	g_return_val_if_fail (name, FALSE);
+
+	g_mutex_lock (xmms_service->mutex);
+	if (!g_hash_table_lookup (xmms_service->registry, name))
+		ret = FALSE;
+	g_mutex_unlock (xmms_service->mutex);
+
+	return ret;
+}
+
+static gboolean
+xmms_service_is_method_registered (xmms_service_entry_t *entry, gchar *name)
+{
+	gboolean ret = TRUE;
+
+	g_return_val_if_fail (entry, FALSE);
+	g_return_val_if_fail (name, FALSE);
+
+	g_mutex_lock (entry->mutex);
+	if (!g_hash_table_lookup (entry->methods, name))
+		ret = FALSE;
+	g_mutex_unlock (entry->mutex);
+
+	return ret;
+}
+
+static xmms_service_entry_t *
+xmms_service_entry_new (gchar *name, gchar *description, guint major,
+						guint minor, guint cookie)
+{
+
+}
+
+static xmms_service_method_t *
+xmms_service_method_new (gchar *name, gchar *description, gchar *ret_type,
+						 guint num_args, gchar *args)
+{
+
 }
 
 /** @} */
