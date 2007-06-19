fcdf0a986a20446537df4e1f18c0d12f89af6c2d
Thomas Coppi <tcoppi@gmail.com>
2007-06-19 16:33:30 -0600

    FEATURE(1600): Output enum header declarations(part 1)

--- src/genipc.py
+++ src/genipc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import string
 from xml.dom.ext.reader.Sax2 import FromXmlStream
 
 #dictionary mapping of types in the xml to C types
@@ -15,11 +16,34 @@ if __name__ == "__main__":
 	doc = FromXmlStream(xmlfile)
 
 	#open up the output ipc.c file
-	ipcfile = open("genipc_out/ipc.c", "w+")
+	ipcfile = open("genipc_out/ipc.c", "w")
+	xmmsclientfile = open("genipc_out/xmmsclient.h", "w");
 
 	nodes = doc.getElementsByTagName("ipc")
-	if nodes[0].tagName == "ipc":
+	if nodes[0].nodeName == "ipc":
 		print "Parsing XMMS2 IPC XML Description file version %s..." % \
 		nodes[0].getAttribute("version")
 	else:
 		print "Error, XML files not IPC Description file."
+
+	#parse all the enumerations
+	print "Parsing enumerations"
+	enum_list = nodes[0].getElementsByTagName("enum")
+
+	for node in enum_list:
+		#if its a child of the main(ipc) tag
+		if node.parentNode == nodes[0]:
+			print "%s" % node.getAttribute("name")
+
+			xmmsclientfile.write("/* %s enum */\n" % \
+					node.getAttribute("name"));
+			xmmsclientfile.write("typedef enum {\n");
+
+			props = node.getElementsByTagName("prop");
+			for prop in props:
+				xmmsclientfile.write("\tXMMSC_%s_%s,\n" % \
+						(node.getAttribute("name").upper(),prop.getAttribute("name")))
+			xmmsclientfile.write("} xmmsc_%s_t; \n" % \
+					node.getAttribute("name"))
+
+
