f7f98f870b1ef53ed5cd9867a7474ea6a3609284
Thomas Coppi <tcoppi@gmail.com>
2007-06-19 12:07:11 -0600

    FEATURE(1600): More generator script template

--- src/genipc.py
+++ src/genipc.py
@@ -3,6 +3,9 @@ import sys
 from xml.dom.ext.reader.Sax2 import FromXmlStream
 
 #load the xml file
-file = open("ipc.xml")
+xmlfile = open("ipc.xml", "r")
 doc = FromXmlStream(file)
+
+#open up the output ipc.c file
+ipcfile = open("src/ipc.c", "w")
 
