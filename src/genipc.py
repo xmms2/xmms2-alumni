c6cd4a2fadd5948ce680aad6d3b02fd9492d43f3
Thomas Coppi <tcoppi@gmail.com>
2007-06-19 15:23:15 -0600

    FEATURE(1600): Add to simple XML prototype, detect if xml is an IPC description

--- src/genipc.py
+++ src/genipc.py
@@ -2,10 +2,24 @@
 import sys
 from xml.dom.ext.reader.Sax2 import FromXmlStream
 
-#load the xml file
-xmlfile = open("ipc.xml", "r")
-doc = FromXmlStream(file)
+#dictionary mapping of types in the xml to C types
+c_map = {}
+c_map["none"] = "void"
+c_map["int"] = "int"
+c_map["string"] = "char *"
 
-#open up the output ipc.c file
-ipcfile = open("src/ipc.c", "w+")
+if __name__ == "__main__":
 
+	#load the xml file
+	xmlfile = open("ipc.xml", "r")
+	doc = FromXmlStream(xmlfile)
+
+	#open up the output ipc.c file
+	ipcfile = open("genipc_out/ipc.c", "w+")
+
+	nodes = doc.getElementsByTagName("ipc")
+	if nodes[0].tagName == "ipc":
+		print "Parsing XMMS2 IPC XML Description file version %s..." % \
+		nodes[0].getAttribute("version")
+	else:
+		print "Error, XML files not IPC Description file."
