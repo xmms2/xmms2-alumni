07de1c9a5af88bd3ff6fe5b4c74a1f453aec6a2f
Thomas Coppi <tcoppi@gmail.com>
2007-06-19 17:19:57 -0600

    FEATURE(1600): Output definition of client objects

--- src/genipc.py
+++ src/genipc.py
@@ -6,8 +6,51 @@ from xml.dom.ext.reader.Sax2 import FromXmlStream
 #dictionary mapping of types in the xml to C types
 c_map = {}
 c_map["none"] = "void"
-c_map["int"] = "int"
+c_map["int"] = "gint"
 c_map["string"] = "char *"
+c_map["enum"] = "guint"
+
+def do_enums(enums):
+	for node in enums:
+		#if its a child of the main(ipc) tag
+		if node.parentNode == nodes[0]:
+			print "%s" % node.getAttribute("name")
+
+			xmmsclientfile.write("/* %s enum */\n" % \
+					node.getAttribute("name"));
+			xmmsclientfile.write("typedef enum {\n");
+
+			props = node.getElementsByTagName("prop");
+			for prop in props:
+				xmmsclientfile.write("\tXMMSC_%s_%s,\n" % \
+						(node.getAttribute("name").upper(),prop.getAttribute("name")))
+			xmmsclientfile.write("} xmmsc_%s_t; \n\n" % \
+					node.getAttribute("name"))
+
+def do_objects(objects):
+	for node in objects:
+		#if its a child of the main(ipc) tag
+		if node.parentNode == nodes[0]:
+			print "%s" % node.getAttribute("name")
+
+			if node.getAttribute("type") == "client":
+				xmmsclientfile.write("\n/* %s object */\n" % \
+						node.getAttribute("name"))
+
+				xmmsclientfile.write("typedef struct {\n");
+
+				props = node.getElementsByTagName("prop");
+				for prop in props:
+					type = node.getElementsByTagName("type");
+					xmmsclientfile.write("\t%s %s;\n" % \
+							(c_map[type[0].childNodes[1].nodeName],prop.getAttribute("name")))
+
+				xmmsclientfile.write("} xmmsc_%s_t; \n" % \
+						node.getAttribute("name"))
+				
+
+			else:
+				print "asdf"
 
 if __name__ == "__main__":
 
@@ -29,21 +72,10 @@ if __name__ == "__main__":
 	#parse all the enumerations
 	print "Parsing enumerations"
 	enum_list = nodes[0].getElementsByTagName("enum")
+	do_enums(enum_list)
 
-	for node in enum_list:
-		#if its a child of the main(ipc) tag
-		if node.parentNode == nodes[0]:
-			print "%s" % node.getAttribute("name")
-
-			xmmsclientfile.write("/* %s enum */\n" % \
-					node.getAttribute("name"));
-			xmmsclientfile.write("typedef enum {\n");
-
-			props = node.getElementsByTagName("prop");
-			for prop in props:
-				xmmsclientfile.write("\tXMMSC_%s_%s,\n" % \
-						(node.getAttribute("name").upper(),prop.getAttribute("name")))
-			xmmsclientfile.write("} xmmsc_%s_t; \n" % \
-					node.getAttribute("name"))
-
+	#parse all objects
+	print "Parsing objects"
+	object_list = nodes[0].getElementsByTagName("object")
+	do_objects(object_list)
 
