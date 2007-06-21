fbb5d3282c52876c3ebe50c63394a9a680903f47
Thomas Coppi <tcoppi@gmail.com>
2007-06-21 15:46:26 -0600

    FEATURE(1600): Add stubs for client-side

---
+++ src/clients/lib/xmmsclient/ipc.c
@@ -0,0 +1,1 @@
+#include "xmmsclient/xmmsclient.h"
---
+++ src/include/xmmsclient/xmmsclient_conn.h
@@ -0,0 +1,16 @@
+#ifndef __XMMS_CLIENT_H__
+#define __XMMS_CLIENT_H__
+
+#include "xmmsc/xmmsc_stdint.h"
+#include "xmmsc/xmmsc_ipc_msg.h"
+#include "xmmsc/xmmsc_idnumbers.h"
+#include "xmmsc/xmmsc_coll.h"
+
+struct xmmsc_connection_t_ {
+
+};
+
+typedef struct xmmsc_connection_t_ xmmsc_connection_t;
+typedef unsigned int xmmsc_error_t;
+
+#endif
