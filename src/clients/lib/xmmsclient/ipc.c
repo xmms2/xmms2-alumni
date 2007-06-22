2ad252865bca87caa35926c4888cacabed11886c
Thomas Coppi <tcoppi@gmail.com>
2007-06-22 14:00:17 -0600

    FEATURE(1600): Initial Request sending/receiving. Next is using the generated vals for IPC commands.

--- src/clients/lib/xmmsclient/ipc.c
+++ src/clients/lib/xmmsclient/ipc.c
@@ -7,11 +7,11 @@ xmmsc_ipc_msg_write(xmmsc_connection_t *c, xmms_ipc_msg_t *msg)
 	bool sent = false;
 
 	while (!sent) {
-		sent = xmms_ipc_msg_write_transport (msg, c->trans, &disconnected);
+		sent = xmms_ipc_msg_write_transport (msg,
+				xmmsc_connection_get_transport (c), &disconnected);
 		if (disconnected)
 			return false; //Should disconnect properly
 	}
 
 	return true;
 }
-
