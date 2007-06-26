5cf1fd12e5bd6ba6437d46d55840e514b5c46496
Ning Shi <zeegeek@gmail.com>
2007-06-26 17:24:15 +0800

    OTHER: Add helper functions in clientlib to deal with service structs.

--- src/clients/lib/xmmsclient/service.c
+++ src/clients/lib/xmmsclient/service.c
@@ -262,15 +262,106 @@ xmmsc_service_list_method_args (xmmsc_connection_t *conn, char *service,
 }
 
 /**
+ * Parse a service's information.
+ *
+ * Caller is responsible for freeing the list and all the elements, or simply
+ * call the helper function #xmmsc_service_free.
+ * 
+ * @param res The #xmmsc_result_t returned by #xmmsc_service_list.
+ * @return The #xmmsc_service_t structure or NULL if failed.
+ */
+xmmsc_service_t *
+xmmsc_service_parse_service (xmmsc_result_t *res)
+{
+	char *name = NULL;
+	char *desc = NULL;
+	xmmsc_service_t *ret = NULL;
+
+	if (xmmsc_result_iserror (res))
+		return ret;
+
+	ret = x_new0 (xmmsc_service_t, 1);
+
+	if (!xmmsc_result_get_dict_entry_string (res, "name", &name))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_string (res, "description", &desc))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_uint (res, "major_version",
+	                                       &ret->major_version))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_uint (res, "minor_version",
+										   &ret->minor_version))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_uint (res, "count", &ret->count))
+		goto err;
+	ret->name = x_new0 (char, strlen (name) + 1);
+	strcpy (ret->name, name);
+	ret->description = x_new0 (char, strlen (desc) + 1);
+	strcpy (ret->description, desc);
+
+	return ret;
+
+err:
+	free (ret);
+	return NULL;
+}
+
+/**
+ * Parse a method's information.
+ *
+ * Caller is responsible for freeing the list and all the elements, or simply
+ * call the helper function #xmmsc_service_free_method.
+ * 
+ * @param res The #xmmsc_result_t returned by #xmmsc_service_list_method.
+ * @return The #xmmsc_service_method_t structure or NULL if failed.
+ */
+xmmsc_service_method_t *
+xmmsc_service_parse_method (xmmsc_result_t *res)
+{
+	char *name = NULL;
+	char *desc = NULL;
+	xmmsc_service_method_t *ret = NULL;
+
+	if (xmmsc_result_iserror (res))
+		return ret;
+
+	ret = x_new0 (xmmsc_service_method_t, 1);
+
+	if (!xmmsc_result_get_dict_entry_string (res, "name", &name))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_string (res, "description", &desc))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_uint (res, "num_rets", &ret->num_rets))
+		goto err;
+	if (!xmmsc_result_get_dict_entry_uint (res, "num_args", &ret->num_args))
+		goto err;
+	ret->name = x_new0 (char, strlen (name) + 1);
+	strcpy (ret->name, name);
+	ret->description = x_new0 (char, strlen (desc) + 1);
+	strcpy (ret->description, desc);
+
+	return ret;
+
+err:
+	free (ret);
+	return NULL;
+}
+
+/**
  * Parse a method's argument types.
  *
+ * Caller is responsible for freeing the list and all the elements, or simply
+ * call the helper function #xmmsc_service_free_args.
+ * 
  * @param res The #xmmsc_result_t returned by #xmmsc_service_list_method_args.
  * @param num The total number of arguments this method has.
- * @return The list of #xmmsc_service_argument_t. Caller is responsible for freeing the list. Elements are owned by result.
+ * @return The list of #xmmsc_service_argument_t or NULL if failed.
  */
 xmmsc_service_argument_t *
 xmmsc_service_parse_arg_types (xmmsc_result_t *res, uint32_t num)
 {
+	char *name = NULL;
+	uint32_t i = num;
 	xmmsc_service_argument_t *ret = NULL;
 
 	if (xmmsc_result_iserror (res) || !xmmsc_result_list_valid (res) || num <= 0)
@@ -278,14 +369,16 @@ xmmsc_service_parse_arg_types (xmmsc_result_t *res, uint32_t num)
 
 	ret = x_new0 (xmmsc_service_argument_t, num);
 
-	while (xmmsc_result_list_valid (res) && num-- > 0) {
-		if (!xmmsc_result_get_dict_entry_string (res, "name", &ret[num].name))
+	while (xmmsc_result_list_valid (res) && i-- > 0) {
+		if (!xmmsc_result_get_dict_entry_string (res, "name", &name))
 			goto err;
-		if (!xmmsc_result_get_dict_entry_uint (res, "type", &ret[num].type))
+		if (!xmmsc_result_get_dict_entry_uint (res, "type", &ret[i].type))
 			goto err;
 		if (!xmmsc_result_get_dict_entry_uint (res, "optional",
-		                                       &ret[num].optional))
+		                                       &ret[i].optional))
 			goto err;
+		ret[i].name = x_new0 (char, strlen (name) + 1);
+		strcpy (ret[i].name, name);
 
 		xmmsc_result_list_next (res);
 	}
@@ -293,8 +386,52 @@ xmmsc_service_parse_arg_types (xmmsc_result_t *res, uint32_t num)
 	return ret;
 
 err:
+	while (++i < num)
+		free (ret[i].name);
 	free (ret);
 	return NULL;
+}
+
+/**
+ * Free the given #xmmsc_service_t.
+ *
+ * @param service The #xmmsc_service_t list.
+ */
+void
+xmmsc_service_free_service (xmmsc_service_t *service)
+{
+	free (service->name);
+	free (service->description);
+	free (service);
+}
+
+/**
+ * Free the given #xmmsc_service_method_t.
+ *
+ * @param method The #xmmsc_service_method_t list.
+ */
+void
+xmmsc_service_free_method (xmmsc_service_method_t *method)
+{
+	free (method->name);
+	free (method->description);
+	free (method);
+}
+
+/**
+ * Free the given argument list.
+ *
+ * @param args The #xmmsc_service_argument_t list.
+ * @param num The total number of arguments in the list.
+ */
+void
+xmmsc_service_free_args (xmmsc_service_argument_t *args, uint32_t num)
+{
+	uint32_t i = num;
+
+	while (i-- > 0)
+		free (args[i].name);
+	free (args);
 }
 
 /* @} */
