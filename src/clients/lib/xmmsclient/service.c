b1ee2166a15d63af840a9f2cdb0fe1da1cd75353
Ning Shi <zeegeek@gmail.com>
2007-06-30 02:20:17 +0800

    OTHER: Change argument list structure to include list size.

--- src/clients/lib/xmmsclient/service.c
+++ src/clients/lib/xmmsclient/service.c
@@ -95,17 +95,17 @@ xmmsc_service_register_full (xmmsc_connection_t *conn,
 		xmms_ipc_msg_put_string (msg, method->name);
 		xmms_ipc_msg_put_string (msg, method->description);
 
-		xmms_ipc_msg_put_uint32 (msg, method->num_rets);
-		for (count = 0; count < method->num_rets; count++) {
-			arg = method->rets + count;
+		xmms_ipc_msg_put_uint32 (msg, method->ret_list->size);
+		for (count = 0; count < method->ret_list->size; count++) {
+			arg = method->ret_list->args + count;
 			xmms_ipc_msg_put_string (msg, arg->name);
 			xmms_ipc_msg_put_uint32 (msg, arg->type);
 			xmms_ipc_msg_put_uint32 (msg, arg->optional);
 		}
 
-		xmms_ipc_msg_put_uint32 (msg, method->num_args);
-		for (count = 0; count < method->num_args; count++) {
-			arg = method->args + count;
+		xmms_ipc_msg_put_uint32 (msg, method->arg_list->size);
+		for (count = 0; count < method->arg_list->size; count++) {
+			arg = method->arg_list->args + count;
 			xmms_ipc_msg_put_string (msg, arg->name);
 			xmms_ipc_msg_put_uint32 (msg, arg->type);
 			xmms_ipc_msg_put_uint32 (msg, arg->optional);
@@ -263,13 +263,13 @@ xmmsc_service_method_args_list (xmmsc_connection_t *conn, const char *service,
  *
  * @param conn The connection to the server.
  * @param service The id of the service.
- * @param method The method structure.
- * @param args The arguments passing to the method.
+ * @param method The id of the method.
+ * @param arg_list The argument list passing to the method.
  */
 xmmsc_result_t *xmmsc_service_request (xmmsc_connection_t *conn,
-                                       const xmmsc_service_t *service,
-                                       const xmmsc_service_method_t *method,
-                                       const xmmsc_service_argument_t *args)
+                                       const char *service,
+                                       const char *method,
+                                       const xmmsc_service_arg_list_t *arg_list)
 {
 	xmms_ipc_msg_t *msg;
 	xmmsc_result_t *res;
@@ -281,30 +281,31 @@ xmmsc_result_t *xmmsc_service_request (xmmsc_connection_t *conn,
 
 	msg = xmms_ipc_msg_new (XMMS_IPC_OBJECT_SERVICE,
 	                        XMMS_IPC_CMD_SERVICE_REQUEST);
-	xmms_ipc_msg_put_string (msg, service->name);
-	xmms_ipc_msg_put_string (msg, method->name);
-	if (args) {
-		for (i = 0; i < method->num_args; i++) {
-			xmms_ipc_msg_put_string (msg, args[i].name);
-			xmms_ipc_msg_put_uint32 (msg, args[i].none);
-			switch (args[i].type) {
+	xmms_ipc_msg_put_string (msg, service);
+	xmms_ipc_msg_put_string (msg, method);
+	if (arg_list) {
+		for (i = 0; i < arg_list->size; i++) {
+			xmms_ipc_msg_put_string (msg, arg_list->args[i].name);
+			xmms_ipc_msg_put_uint32 (msg, arg_list->args[i].none);
+			switch (arg_list->args[i].type) {
 			case XMMSC_SERVICE_ARG_TYPE_UINT32:
-				xmms_ipc_msg_put_uint32 (msg, args[i].value.uint32);
+				xmms_ipc_msg_put_uint32 (msg, arg_list->args[i].value.uint32);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_INT32:
-				xmms_ipc_msg_put_int32 (msg, args[i].value.int32);
+				xmms_ipc_msg_put_int32 (msg, arg_list->args[i].value.int32);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRING:
-				xmms_ipc_msg_put_string (msg, args[i].value.string);
+				xmms_ipc_msg_put_string (msg, arg_list->args[i].value.string);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRINGLIST:
-				xmms_ipc_msg_put_string_list (msg, (const char **)args[i].value.strings);
+				xmms_ipc_msg_put_string_list (msg, (const char **)arg_list->args[i].value.strings);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_COLL:
-				xmms_ipc_msg_put_collection (msg, args[i].value.coll);
+				xmms_ipc_msg_put_collection (msg, arg_list->args[i].value.coll);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_BIN:
-				xmms_ipc_msg_put_bin (msg, args[i].value.bin, args[i].len);
+				xmms_ipc_msg_put_bin (msg, arg_list->args[i].value.bin,
+				                      arg_list->args[i].len);
 				break;
 			default:
 				return NULL;
@@ -322,46 +323,49 @@ xmmsc_result_t *xmmsc_service_request (xmmsc_connection_t *conn,
  * Return service method request.
  *
  * @param conn The connection to the server.
- * @param cookie The service method request cookie.
- * @param method The method structure.
- * @param args The returning arguments.
+ * @param res The result containing the service cookie.
+ * @param arg_list The returning argument list.
  */
 xmmsc_result_t *xmmsc_service_return (xmmsc_connection_t *conn,
-                                      uint32_t cookie,
-                                      const xmmsc_service_method_t *method,
-                                      const xmmsc_service_argument_t *args)
+                                      xmmsc_result_t *res,
+                                      const xmmsc_service_arg_list_t *arg_list)
 {
 	xmms_ipc_msg_t *msg;
+	uint32_t cookie;
 	int i;
 
 	x_check_conn (conn, NULL);
-	x_return_val_if_fail (method, NULL);
+	x_return_val_if_fail (res, NULL);
+
+	if (!xmmsc_result_get_service_cookie (res, &cookie))
+		return NULL;
 
 	msg = xmms_ipc_msg_new (XMMS_IPC_OBJECT_SERVICE,
 	                        XMMS_IPC_CMD_SERVICE_RETURN);
 	xmms_ipc_msg_put_uint32 (msg, cookie);
-	if (args) {
-		for (i = 0; i < method->num_rets; i++) {
-			xmms_ipc_msg_put_string (msg, args[i].name);
-			xmms_ipc_msg_put_uint32 (msg, args[i].none);
-			switch (args[i].type) {
+	if (arg_list->args) {
+		for (i = 0; i < arg_list->size; i++) {
+			xmms_ipc_msg_put_string (msg, arg_list->args[i].name);
+			xmms_ipc_msg_put_uint32 (msg, arg_list->args[i].none);
+			switch (arg_list->args[i].type) {
 			case XMMSC_SERVICE_ARG_TYPE_UINT32:
-				xmms_ipc_msg_put_uint32 (msg, args[i].value.uint32);
+				xmms_ipc_msg_put_uint32 (msg, arg_list->args[i].value.uint32);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_INT32:
-				xmms_ipc_msg_put_int32 (msg, args[i].value.int32);
+				xmms_ipc_msg_put_int32 (msg, arg_list->args[i].value.int32);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRING:
-				xmms_ipc_msg_put_string (msg, args[i].value.string);
+				xmms_ipc_msg_put_string (msg, arg_list->args[i].value.string);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRINGLIST:
-				xmms_ipc_msg_put_string_list (msg, (const char **)args[i].value.strings);
+				xmms_ipc_msg_put_string_list (msg, (const char **)arg_list->args[i].value.strings);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_COLL:
-				xmms_ipc_msg_put_collection (msg, args[i].value.coll);
+				xmms_ipc_msg_put_collection (msg, arg_list->args[i].value.coll);
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_BIN:
-				xmms_ipc_msg_put_bin (msg, args[i].value.bin, args[i].len);
+				xmms_ipc_msg_put_bin (msg, arg_list->args[i].value.bin,
+				                      arg_list->args[i].len);
 				break;
 			default:
 				return NULL;
@@ -411,15 +415,15 @@ xmmsc_service_new (const char *name, const char *description,
  *
  * @param name Method name (will be duplicated).
  * @param description Method description (will be duplicated).
- * @param rets Pointer to return #xmmsc_service_argument_t.
- * @param args Pointer to argument list #xmmsc_service_argument_t.
+ * @param rets Pointer to return #xmmsc_service_arg_list_t.
+ * @param args Pointer to argument list #xmmsc_service_arg_list_t.
  * @param func Callback function.
  * @return The newly created #xmmsc_service_method_t.
  */
 xmmsc_service_method_t *
 xmmsc_service_method_new (const char *name, const char *description,
-                          uint32_t num_rets, xmmsc_service_argument_t *rets,
-                          uint32_t num_args, xmmsc_service_argument_t *args,
+                          xmmsc_service_arg_list_t *rets,
+                          xmmsc_service_arg_list_t *args,
                           xmmsc_result_notifier_t func)
 {
 	xmmsc_service_method_t *ret = NULL;
@@ -431,10 +435,8 @@ xmmsc_service_method_new (const char *name, const char *description,
 
 	ret->name = strdup (name);
 	ret->description = strdup (description);
-	ret->num_rets = num_rets;
-	ret->rets = rets;
-	ret->num_args = num_args;
-	ret->args = args;
+	ret->ret_list = rets;
+	ret->arg_list = args;
 	ret->func = func;
 
 	return ret;
@@ -469,16 +471,16 @@ xmmsc_service_method_free (xmmsc_service_method_t *method)
 /**
  * Free the given argument list.
  *
- * @param args The #xmmsc_service_argument_t list.
- * @param num The total number of arguments in the list.
+ * @param args The #xmmsc_service_arg_list_t list.
  */
 void
-xmmsc_service_args_free (xmmsc_service_argument_t *args, uint32_t num)
+xmmsc_service_args_free (xmmsc_service_arg_list_t *args)
 {
-	uint32_t i = num;
+	uint32_t i = args->size;
 
 	while (i-- > 0)
-		free (args[i].name);
+		free (args->args[i].name);
+	free (args->args);
 	free (args);
 }
 
@@ -570,14 +572,10 @@ xmmsc_service_method_attribute_get (xmmsc_service_method_t *method,
 		*(char **)value = method->name;
 	else if (strcasecmp (key, "description") == 0)
 		*(char **)value = method->description;
-	else if (strcasecmp (key, "num_rets") == 0)
-		*(uint32_t *)value = method->num_rets;
-	else if (strcasecmp (key, "rets") == 0)
-		*(xmmsc_service_argument_t **)value = method->rets;
-	else if (strcasecmp (key, "num_args") == 0)
-		*(uint32_t *)value = method->num_args;
-	else if (strcasecmp (key, "args") == 0)
-		*(xmmsc_service_argument_t **)value = method->args;
+	else if (strcasecmp (key, "ret_list") == 0)
+		*(xmmsc_service_arg_list_t **)value = method->ret_list;
+	else if (strcasecmp (key, "arg_list") == 0)
+		*(xmmsc_service_arg_list_t **)value = method->arg_list;
 	else if (strcasecmp (key, "func") == 0)
 		*(xmmsc_result_notifier_t *)value = method->func;
 	else
@@ -606,14 +604,10 @@ xmmsc_service_method_attribute_set (xmmsc_service_method_t *method,
 		method->name = *(char **)value;
 	else if (strcasecmp (key, "description") == 0)
 		method->description = *(char **)value;
-	else if (strcasecmp (key, "num_rets") == 0)
-		method->num_rets = *(uint32_t *)value;
-	else if (strcasecmp (key, "rets") == 0)
-		method->rets = *(xmmsc_service_argument_t **)value;
-	else if (strcasecmp (key, "num_args") == 0)
-		method->num_args = *(uint32_t *)value;
-	else if (strcasecmp (key, "args") == 0)
-		method->args = *(xmmsc_service_argument_t **)value;
+	else if (strcasecmp (key, "ret_list") == 0)
+		method->ret_list = *(xmmsc_service_arg_list_t **)value;
+	else if (strcasecmp (key, "arg_list") == 0)
+		method->arg_list = *(xmmsc_service_arg_list_t **)value;
 	else if (strcasecmp (key, "func") == 0)
 		method->func = *(xmmsc_result_notifier_t *)value;
 	else
@@ -625,41 +619,41 @@ xmmsc_service_method_attribute_set (xmmsc_service_method_t *method,
 /**
  * Get an argument value of a method.
  *
- * @param method The method containing the argument.
+ * @param arg_list The argument list containing the argument.
  * @param key The name of the argument.
  * @param value Pointer to the place where the value will be stored to.
  * @return 1 for success, 0 otherwise.
  */
 int
-xmmsc_service_argument_get (xmmsc_service_method_t *method, const char *key,
+xmmsc_service_argument_get (xmmsc_service_arg_list_t *arg_list, const char *key,
                             void *value)
 {
 	int i;
 
-	x_return_val_if_fail (method, 0);
+	x_return_val_if_fail (arg_list, 0);
 	x_return_val_if_fail (key, 0);
 	x_return_val_if_fail (value, 0);
 
-	for (i = 0; i < method->num_args; i++) {
-		if (strcasecmp(method->args[i].name, key) == 0) {
-			switch (method->args[i].type) {
+	for (i = 0; i < arg_list->size; i++) {
+		if (strcasecmp(arg_list->args[i].name, key) == 0) {
+			switch (arg_list->args[i].type) {
 			case XMMSC_SERVICE_ARG_TYPE_UINT32:
-				*(uint32_t *)value = method->args[i].value.uint32;
+				*(uint32_t *)value = arg_list->args[i].value.uint32;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_INT32:
-				*(int32_t *)value = method->args[i].value.int32;
+				*(int32_t *)value = arg_list->args[i].value.int32;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRING:
-				*(char **)value = method->args[i].value.string;
+				*(char **)value = arg_list->args[i].value.string;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRINGLIST:
-				*(char ***)value = method->args[i].value.strings;
+				*(char ***)value = arg_list->args[i].value.strings;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_COLL:
-				*(xmmsc_coll_t **)value = method->args[i].value.coll;
+				*(xmmsc_coll_t **)value = arg_list->args[i].value.coll;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_BIN:
-				*(unsigned char **)value = method->args[i].value.bin;
+				*(unsigned char **)value = arg_list->args[i].value.bin;
 				break;
 			default:
 				return 0;
@@ -675,41 +669,41 @@ xmmsc_service_argument_get (xmmsc_service_method_t *method, const char *key,
 /**
  * Set an argument value of a method.
  *
- * @param method The method containing the argument.
+ * @param arg_list The argument list containing the argument.
  * @param key The name of the argument.
  * @param value The new value.
  * @return 1 for success, 0 otherwise.
  */
 int
-xmmsc_service_argument_set (xmmsc_service_method_t *method, const char *key,
+xmmsc_service_argument_set (xmmsc_service_arg_list_t *arg_list, const char *key,
                             const void *value)
 {
 	int i;
 
-	x_return_val_if_fail (method, 0);
+	x_return_val_if_fail (arg_list, 0);
 	x_return_val_if_fail (key, 0);
 	x_return_val_if_fail (value, 0);
 
-	for (i = 0; i < method->num_args; i++) {
-		if (strcasecmp(method->args[i].name, key) == 0) {
-			switch (method->args[i].type) {
+	for (i = 0; i < arg_list->size; i++) {
+		if (strcasecmp(arg_list->args[i].name, key) == 0) {
+			switch (arg_list->args[i].type) {
 			case XMMSC_SERVICE_ARG_TYPE_UINT32:
-				method->args[i].value.uint32 = *(uint32_t *)value;
+				arg_list->args[i].value.uint32 = *(uint32_t *)value;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_INT32:
-				method->args[i].value.int32 = *(int32_t *)value;
+				arg_list->args[i].value.int32 = *(int32_t *)value;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRING:
-				method->args[i].value.string = (char *)value;
+				arg_list->args[i].value.string = (char *)value;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_STRINGLIST:
-				method->args[i].value.strings = (char **)value;
+				arg_list->args[i].value.strings = (char **)value;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_COLL:
-				method->args[i].value.coll = (xmmsc_coll_t *)value;
+				arg_list->args[i].value.coll = (xmmsc_coll_t *)value;
 				break;
 			case XMMSC_SERVICE_ARG_TYPE_BIN:
-				method->args[i].value.bin = (unsigned char *)value;
+				arg_list->args[i].value.bin = (unsigned char *)value;
 				break;
 			default:
 				return 0;
