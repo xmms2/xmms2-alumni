0c503f16dbb19b444db49ea8b93c6bc8da9f6b9d
Thomas Coppi <tcoppi@gmail.com>
2007-06-29 15:56:41 -0600

    FEATURE(1600): Make client-side code send properly formatted IPC messages.

--- src/include/xmmsclient/xmmsclient_conn.h
+++ src/include/xmmsclient/xmmsclient_conn.h
@@ -25,6 +25,9 @@ typedef struct xmmsc_request_t_ {
 
 	xmmsc_request_callback_t cb;
 	xmmsc_request_interval_t interval;
+
+	unsigned int type;
+
 } xmmsc_request_t;
 
 typedef unsigned int xmmsc_error_t;
@@ -39,6 +42,7 @@ int xmmsc_ipc_msg_write (xmmsc_connection_t *c, xmms_ipc_msg_t *msg);
 xmmsc_request_t *xmmsc_request_new (xmmsc_connection_t *c, xmms_ipc_msg_t *msg);
 void xmmsc_request_set_callback (xmmsc_request_t *req, xmmsc_request_callback_t cb);
 int xmmsc_request_send (xmmsc_request_t *req);
+void xmmsc_request_now (xmmsc_request_t *req);
 
 xmmsc_request_t *xmmsc_object_property_get (xmmsc_connection_t *c, int object,
                  int property);
