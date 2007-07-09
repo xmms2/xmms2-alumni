b9c06077dc02caa70b8a8462f877fc0331c0af20
Thomas Coppi <tcoppi@gmail.com>
2007-07-09 10:31:27 -0600

    OTHER: Update genipc_install.sh to include new files

--- src/genipc_install.sh
+++ src/genipc_install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+#source
 cp genipc_out/ipc_cmds.c xmms
+cp genipc_out/ipc_msg_gen.c xmms
+cp genipc_out/ipc_msg_deserialize.c xmms
+
+#headers
 cp genipc_out/xmmsclient.h include/xmmsclient
 cp genipc_out/*cmds.h includepriv/xmmspriv
 
