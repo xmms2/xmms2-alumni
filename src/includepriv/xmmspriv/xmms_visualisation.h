1dad8c2b45e56f8c81bf723a4d3e5ec082d62903
Johannes Michael Jordan <xmms2@lanrules.de>
2007-08-06 03:06:45 +0200

    FEATURE(1338): Visualisation using UDP
    Only if an attempt to use SHM fails, UDP is used as
    transport. UDP tries to connect to the host provided
    in the server url; or to "localhost"

--- src/includepriv/xmmspriv/xmms_visualisation.h
+++ src/includepriv/xmmspriv/xmms_visualisation.h
@@ -34,6 +34,7 @@ typedef struct xmms_visualisation_St xmms_visualisation_t;
 uint32_t xmms_visualisation_version (xmms_visualisation_t *vis, xmms_error_t *err);
 int32_t xmms_visualisation_register_client (xmms_visualisation_t *vis, xmms_error_t *err);
 int32_t xmms_visualisation_init_shm (xmms_visualisation_t *vis, int32_t id, int32_t shmid, xmms_error_t *err);
+int32_t xmms_visualisation_init_udp (xmms_visualisation_t *vis, int32_t id, xmms_error_t *err);
 int32_t xmms_visualisation_property_set (xmms_visualisation_t *vis, int32_t id, gchar *key, gchar *value, xmms_error_t *err);
 
 /* TODO: do this right */
