0b7151ded05599d8f474f94610a237a0b36b0ea9
Sivert Berg <sivertb@stud.ntnu.no>
2009-06-23 18:24:52 +0200

    Regexp querying
    Made it possible to search for regexps. There seems to be some issues
    with the collection parser though, it adds * before and after regexps
    (I guess to please the GLOB in sqlite). That doesn't work too well with
    regexp matching.

--- src/query.c
+++ src/query.c
@@ -1,6 +1,8 @@
 #include "s4.h"
+#include "s4_be.h"
 #include <xmmsclient/xmmsclient.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 s4_set_t *_coll_to_set (s4_t *s4, xmmsv_coll_t *coll)
@@ -9,13 +11,10 @@ s4_set_t *_coll_to_set (s4_t *s4, xmmsv_coll_t *coll)
 	xmmsv_list_iter_t *it;
 	xmmsv_t *v;
 	xmmsv_coll_t *c;
-	xmmsv_dict_iter_t *dt;
-
-	const char *foo, *bar, *key, *val;
+	const char *key, *val;
 	s4_entry_t *entry;
 
 	xmmsv_get_list_iter (xmmsv_coll_operands_get (coll), &it);
-	xmmsv_get_dict_iter (xmmsv_coll_attributes_get (coll), &dt);
 
 	ret = NULL;
 
@@ -27,8 +26,6 @@ s4_set_t *_coll_to_set (s4_t *s4, xmmsv_coll_t *coll)
 				sa = ret;
 				sb = _coll_to_set (s4, c);
 				ret = s4_set_union (sa, sb);
-				s4_set_free (sa);
-				s4_set_free (sb);
 			}
 			break;
 
@@ -41,8 +38,6 @@ s4_set_t *_coll_to_set (s4_t *s4, xmmsv_coll_t *coll)
 				if (ret != NULL) {
 					sa = ret;
 					ret = s4_set_intersection (sa, sb);
-					s4_set_free (sa);
-					s4_set_free (sb);
 				}
 				else
 					ret = sb;
@@ -50,20 +45,48 @@ s4_set_t *_coll_to_set (s4_t *s4, xmmsv_coll_t *coll)
 			break;
 
 		case XMMS_COLLECTION_TYPE_EQUALS:
-			for (; xmmsv_dict_iter_valid (dt); xmmsv_dict_iter_next (dt)) {
-				xmmsv_dict_iter_pair (dt, &foo, &v);
-				xmmsv_get_string (v, &bar);
+			xmmsv_dict_get (xmmsv_coll_attributes_get (coll), "field", &v);
+			xmmsv_get_string (v, &key);
 
-				if (strcmp (foo, "field") == 0)
-					key = bar;
-				else if (strcmp (foo, "value") == 0)
-					val = bar;
+			xmmsv_dict_get (xmmsv_coll_attributes_get (coll), "value", &v);
+			xmmsv_get_string (v, &val);
 
-			}
 			printf ("Looking for (%s, %s)\n", key, val);
 			entry = s4_entry_get_s (s4, key, val);
 			ret = s4_entry_contained (s4, entry);
 			s4_entry_free (entry);
+			break;
+
+		case XMMS_COLLECTION_TYPE_MATCH:
+			xmmsv_dict_get (xmmsv_coll_attributes_get (coll), "field", &v);
+			xmmsv_get_string (v, &key);
+
+			xmmsv_dict_get (xmmsv_coll_attributes_get (coll), "value", &v);
+			xmmsv_get_string (v, &val);
+
+			/* Get rid of prefix and suffix *, they break the regexp */
+			char *pat = strdup (val + 1);
+			pat[strlen (pat) - 1] = '\0';
+
+			xmmsv_t *list = s4be_st_regexp (s4->be, pat);
+			free (pat);
+			xmmsv_t *str;
+			xmmsv_list_iter_t *it;
+			const char *s;
+
+			xmmsv_get_list_iter (list, &it);
+			while (xmmsv_list_iter_valid (it)) {
+				xmmsv_list_iter_entry (it, &str);
+				xmmsv_get_string (str, &s);
+
+				entry = s4_entry_get_s (s4, key, s);
+				sa = ret;
+				sb = s4_entry_contained (s4, entry);
+				ret = s4_set_union (sa, sb);
+				s4_entry_free (entry);
+
+				xmmsv_list_iter_next (it);
+			}
 			break;
 
 		default:
