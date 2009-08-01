2a98269f7c2f436fb7faa4c391f1ed3b369592db
Tilman Sauerbeck <tilman@xmms.org>
2009-08-01 22:51:40 +0200

    FEATURE(1600): Added a preliminary Ruby bindings code generator.

---
+++ src/clients/lib/ruby2/client.rb
@@ -0,0 +1,197 @@
+#  XMMS2 - X Music Multiplexer System
+#  Copyright (C) 2003-2008 XMMS2 Team
+#
+#  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+
+require 'socket'
+
+module Xmms::Client
+
+	class MethodGroup
+		def initialize(client)
+			@client = client
+		end
+	end
+
+	class Client
+		MAX_COOKIE = 2 ** 29
+		PROTOCOL_VERSION = 16
+
+		attr_reader :name, :socket,
+		            :playlist, :config, :playback, :medialib, :collection,
+		            :mediainfo_reader, :visualization, :xform, :bindata
+
+		def initialize(name)
+			@name = name
+
+			@main = Main.new self
+			@playlist = Playlist.new self
+			@config = Config.new self
+			@playback = Playback.new self
+			@medialib = Medialib.new self
+			@collection = Collection.new self
+			@mediainfo_reader = MediainfoReader.new self
+			@visualization = Visualization.new self
+			@xform = Xform.new self
+			@bindata = Bindata.new self
+
+			@socket = nil
+			@next_cookie = 0
+
+			# results that we're waiting on
+			@results = []
+
+			# messages waiting to be written
+			@send_queue = []
+
+			# the message currently being read
+			@read_message = nil
+		end
+
+		def connect(ipc_path = ENV['XMMS_PATH'])
+			ipc_path = ipc_path.to_str
+
+			# parse an expression like 'tcp://host:port'
+			re = /^(\w+):\/\/([^:]+)(:(\d+))?$/
+			md = re.match(ipc_path)
+
+			protocol = md.captures.first
+
+			case protocol
+			when 'tcp'
+				host = md.captures[1]
+				port = md.captures[3].to_i
+
+				@socket = TCPSocket.new(host, port)
+			when 'unix'
+				@socket = UNIXSocket.new md.captures[1]
+			else
+				raise "invalid protocol -- #{protocol}"
+			end
+
+			result = @main.hello PROTOCOL_VERSION, @name
+			result.wait
+
+			nil
+		end
+
+		def on_disconnect(&block)
+			@disconnect_block = block
+		end
+
+		def quit
+			@main.quit
+		end
+
+		def stats
+			@main.stats
+		end
+
+		def plugin_list
+			@main.plugin_list 0
+		end
+
+		def send_message(message)
+			message.assemble get_next_cookie
+
+			@send_queue << message
+
+			result = Result.new(self, message.cookie)
+			@results << result
+			result
+		end
+
+		# FIXME: code duplication
+		def wait_for(result)
+			flush_send_queue
+
+			loop do
+				message = Message.new
+
+				begin
+					done = message.read_chunk @socket
+				end until done
+
+				process_message message
+
+				break if message.cookie == result.cookie
+			end
+		end
+
+		protected
+		def io_in_handle
+			@read_message ||= Message.new
+
+			done = @read_message.read_chunk @socket
+
+			if done
+				process_message @read_message
+				@read_message = nil
+			end
+		end
+
+		def process_message(message)
+			# find the result that this message refers to
+			index = @results.find_index do |result|
+				result.cookie == message.cookie
+			end
+
+			if index.nil?
+				raise 'cannot find result for cookie'
+			end
+
+			found = @results[index]
+			can_be_invoked_again = found.process_reply message
+
+			unless can_be_invoked_again
+				@results.delete_at index
+			end
+		end
+
+		def io_out_handle
+			message = @send_queue.first
+
+			# write the next chunk of the message
+			done = message.write_chunk @socket
+
+			# if this message was written completely,
+			# we remove it from the queue.
+			if done
+				@send_queue.shift
+			end
+		end
+
+		def io_want_out
+			!@send_queue.empty?
+		end
+
+		private
+		def get_next_cookie
+			cookie = @next_cookie
+
+			if @next_cookie == MAX_COOKIE
+				@next_cookie = 0
+			else
+				@next_cookie += 1
+			end
+
+			cookie
+		end
+
+		def flush_send_queue
+			while io_want_out
+				io_out_handle
+			end
+		end
+	end
+end
---
+++ src/clients/lib/ruby2/message.rb
@@ -0,0 +1,157 @@
+#  XMMS2 - X Music Multiplexer System
+#  Copyright (C) 2003-2008 XMMS2 Team
+#
+#  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+
+module Xmms
+	module Client
+	end
+end
+
+module Xmms::Client
+	class Message
+		HEADER_LENGTH = 16
+
+		attr_accessor :object_id, :command_id
+		attr_reader :cookie
+
+		def initialize
+			@object_id = nil
+			@command_id = nil
+			@cookie = nil
+			@payload_length = 0
+
+			@transferred = 0
+
+			@payload_offset = HEADER_LENGTH
+			@raw_data = ''
+		end
+
+		# returns false if more data needs to be written
+		def write_chunk(socket)
+			left = HEADER_LENGTH + @payload_length - @transferred
+			chunk = @raw_data[@transferred, left]
+			written = socket.syswrite chunk
+
+			@transferred += written
+			left -= written
+
+			left.zero?
+		end
+
+		def read_chunk(socket)
+			if @transferred < HEADER_LENGTH
+				chunk = socket.sysread HEADER_LENGTH - @transferred
+				@raw_data << chunk
+				@transferred += chunk.size
+
+				if @transferred == HEADER_LENGTH
+					items = @raw_data.unpack 'NNNN'
+
+					@object_id = items.shift
+					@command_id = items.shift
+					@cookie = items.shift
+					@payload_length = items.shift
+				end
+			end
+
+			total_length = HEADER_LENGTH + @payload_length
+
+			chunk = socket.sysread total_length - @transferred
+			@raw_data << chunk
+			@transferred += chunk.size
+
+			@transferred == total_length
+		end
+
+		def assemble(cookie)
+			@cookie = cookie
+
+			header = [
+				@object_id, @command_id, @cookie, @payload_length
+			].pack 'NNNN'
+
+			@raw_data = header << @raw_data
+		end
+
+		def self.check_int(n)
+			n = n.to_int
+
+			if n > 0x7fffffff || n < -0x80000000
+				raise ArgumentError, 'expected 32 bit signed integer'
+			end
+
+			n
+		end
+
+		def write_int(n)
+			chunk = [n].pack 'N'
+			@raw_data << chunk
+
+			@payload_length += 4
+
+			self
+		end
+
+		def self.check_string(s)
+			s.to_str
+		end
+
+		def write_string(s)
+			z_length = s.length + 1
+
+			write_int z_length
+
+			@raw_data << s << "\0"
+
+			@payload_length += z_length
+
+			self
+		end
+
+		def self.check_list(list, klass)
+			list.each do |item|
+				unless item.class == klass
+					raise TypeError,
+					      "expected #{klass} but got #{item.class}"
+				end
+			end
+		end
+
+		def write_list(list)
+			Value.serialize(list, self)
+
+			self
+		end
+
+		def read_int
+			value = @raw_data[@payload_offset, 4].unpack("N").first
+			@payload_offset += 4
+
+			value
+		end
+
+		def read_string
+			length = read_int
+
+			if length.zero?
+				""
+			else
+				s = @raw_data[@payload_offset, length - 1]
+				@payload_offset += length
+
+				s
+			end
+		end
+	end
+end
