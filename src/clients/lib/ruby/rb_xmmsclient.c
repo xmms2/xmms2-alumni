d9c4b4786566c02ea61cf132d6a737b11205c5c4
Daniel Svensson <dsvensson@gmail.com>
2012-06-06 23:08:35 +0200

    OTHER: Remove remaining uint legacy from ruby bindings.

--- src/clients/lib/ruby/rb_xmmsclient.c
+++ src/clients/lib/ruby/rb_xmmsclient.c
@@ -606,7 +606,7 @@ c_playback_volume_set (VALUE self, VALUE channel, VALUE volume)
 
 	res = xmmsc_playback_volume_set (xmms->real,
 	                                 rb_id2name (SYM2ID (channel)),
-	                                 NUM2UINT (volume));
+	                                 NUM2INT (volume));
 
 	return TO_XMMS_CLIENT_RESULT (self, res);
 }
@@ -802,8 +802,7 @@ c_medialib_entry_property_set (int argc, VALUE *argv, VALUE self)
 	xmmsc_result_t *res;
 	const char *ckey;
 	bool is_str = false;
-	uint32_t id;
-	int32_t ivalue;
+	int32_t id, ivalue;
 
 	Data_Get_Struct (self, RbXmmsClient, xmms);
 
@@ -1306,8 +1305,8 @@ c_coll_query_ids (int argc, VALUE *argv, VALUE self)
 	res = xmmsc_coll_query_ids (xmms->real,
 	                            FROM_XMMS_CLIENT_COLLECTION (coll),
 	                            corder,
-	                            NIL_P (start) ? 0 : NUM2UINT (start),
-	                            NIL_P (start) ? 0 : NUM2UINT (len));
+	                            NIL_P (start) ? 0 : NUM2INT (start),
+	                            NIL_P (start) ? 0 : NUM2INT (len));
 
 	if (corder)
 		xmmsv_unref (corder);
@@ -1347,8 +1346,8 @@ c_coll_query_info (int argc, VALUE *argv, VALUE self)
 	res = xmmsc_coll_query_infos (xmms->real,
 	                            FROM_XMMS_CLIENT_COLLECTION (coll),
 	                            corder,
-	                            NIL_P (start) ? 0 : NUM2UINT (start),
-	                            NIL_P (start) ? 0 : NUM2UINT (len),
+	                            NIL_P (start) ? 0 : NUM2INT (start),
+	                            NIL_P (start) ? 0 : NUM2INT (len),
 	                            cfetch,
 	                            cgroup);
 	xmmsv_unref (cfetch);
